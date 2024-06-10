f0ca7bd4e6d78a34a6f4e80b85e7c4465d422e34
catri-01 <catrimaouene@gmail.com>
2024-06-10 07:19:03 +0200

    add the loops to verify command

--- spaceshipFactory/Program.cs
+++ spaceshipFactory/Program.cs
@@ -12,7 +12,19 @@ namespace spaceshipFactory
             InventoryManager inventory = InventoryManager.Instance;
             ShipAssembler assembler = new ShipAssembler(inventory);
 
+			Console.WriteLine("Welcome to the spaceship factory!");
+			Console.WriteLine("Available commands: ADD_PART <part_name> <quantity>, ADD_SHIP <ship_type> <quantity>, DISPLAY_PARTS, DISPLAY_SHIPS, EXIT");
+
             while (true)
+            {
+                Console.Write("> ");
+                string command = Console.ReadLine();
+                if (command.ToUpper() == "EXIT")
+                    break;
+                parser.ParseAndExecute(command, inventory,assembler);
+            }
+
+			while (true)
             {
                 try
                 {
