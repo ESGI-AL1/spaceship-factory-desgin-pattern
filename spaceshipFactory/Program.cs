fe13a1ca8b82b2f5d7dd875c8d495369b33e8393
xororist <hcachon@myges.fr>
2024-07-12 10:07:48 +0200

    Refactorisation

--- spaceshipFactory/Program.cs
+++ spaceshipFactory/Program.cs
@@ -1,40 +1,21 @@
-﻿namespace spaceshipFactory
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            InventoryManager inventory = InventoryManager.Instance;
-            ShipAssembler assembler = new ShipAssembler(inventory);
-            Stock stock = Stock.GetInstance();
+﻿using spaceshipFactory;
+using spaceshipFactory.Commands;
+using spaceshipFactory.Storage;
+
+Stock stock = Stock.GetInstance();
+CommandParser commandParser = new CommandParser(stock);
             
-            stock.InitStock();
+stock.InitStock();
 
-			Console.WriteLine("Welcome to the spaceship factory!");
-			Console.WriteLine("Available commands: ADD_PART <part_name> <quantity>, ADD_SHIP <ship_type> <quantity>, DISPLAY_PARTS, DISPLAY_SHIPS, EXIT");
-
-            while (true)
-            {
-                Console.Write("> ");
-                string? command = Console.ReadLine();
-                if (command is not null)
-                {
-                    var result = command?
-                        .Split(',')
-                        .SelectMany(cmd => cmd.Trim().Split(' '))
-                        .ToArray();
-                    
-                    foreach (var commands in result)
-                    {
-                        Console.WriteLine(commands);
-                    }
-                    
-                    CommandParser.ParseAndExecute(result);
-                }
-
-                if (command?.ToUpper() == "EXIT")
-                    break;
-            }
-        }
+while (true)
+{
+    Console.Write("> ");
+    string? command = Console.ReadLine();
+    if (command is not null)
+    {
+        commandParser.ExecuteCommand(command);
     }
+
+    if (command?.ToUpper() == "EXIT")
+        break;
 }
