6ab748c254cfb8bb0a114253afad3843a661a80b
xororist <hcachon@myges.fr>
2024-07-17 10:53:01 +0200

    Fix on the Verify Command

--- spaceshipFactory/Commands/VerifyCommand.cs
+++ spaceshipFactory/Commands/VerifyCommand.cs
@@ -5,10 +5,9 @@ namespace spaceshipFactory.Commands;
 public class VerifyCommand(Dictionary<string, int> command) : ICommand
 {
     private readonly Stock _stock = Stock.GetInstance();
-    private Dictionary<string, int> Command { get; } = command;
 
     public void Execute()
     {
-        Console.WriteLine(_stock.VerifyCommand(Command) ? "AVAILABLE" : "UNAVAILABLE");
+        Console.WriteLine(_stock.VerifyCommand(command) ? "AVAILABLE" : "UNAVAILABLE");
     }
 }
