5500ab712c7c25f03921ed5369422a4ad5fcae76
xororist <hcachon@myges.fr>
2024-07-18 13:40:19 +0200

    Memento v1

---
+++ spaceshipFactory/History/StockMemento.cs
@@ -0,0 +1,7 @@
+﻿namespace spaceshipFactory.History;
+
+public class StockMemento(Dictionary<string, int> pieces, Dictionary<string, int> spaceships)
+{
+    public Dictionary<string, int> Pieces { get; } = new(pieces);
+    public Dictionary<string, int> Spaceships { get; } = new(spaceships);
+}
