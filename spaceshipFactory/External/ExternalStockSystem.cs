b541e3859913d4630708aef3517a43f006cf2fe2
catri-01 <catrimaouene@gmail.com>
2024-07-20 15:04:42 +0200

    add the adapter stock

---
+++ spaceshipFactory/External/ExternalStockSystem.cs
@@ -0,0 +1,17 @@
+﻿namespace spaceshipFactory.External
+{
+	public class ExternalStockSystem
+	{
+		public List<(string Item, int Quantity)> GetExternalStock()
+		{
+			// Simule le retour d'un stock externe
+			return new List<(string Item, int Quantity)>
+			{
+				("Hull_HC1", 5),
+				("Engine_EC1", 5),
+				("Wings_WC1", 5),
+				("Thruster_TC1", 5)
+			};
+		}
+	}
+}
