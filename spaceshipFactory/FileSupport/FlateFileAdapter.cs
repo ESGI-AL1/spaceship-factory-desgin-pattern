02bca95598655eac11b736acb643d35d41d638a2
xororist <hcachon@myges.fr>
2024-07-20 22:55:39 +0200

    Fix on adapter to load files

--- spaceshipFactory/FileSupport/FlateFileAdapter.cs
+++ spaceshipFactory/FileSupport/FlateFileAdapter.cs
@@ -1,6 +1,14 @@
 namespace spaceshipFactory.FileSupport;
 
-public class FlateFileAdapter
+public class FlatFileAdapter : IFileAdapter
 {
-    
+    public List<string> LoadCommands(string filePath)
+    {
+        return [..File.ReadAllLines(filePath)];
+    }
+
+    public void SaveOutput(string filePath, List<string> output)
+    {
+        File.WriteAllLines(filePath, output);
+    }
 }
--- spaceshipFactory/FileSupport/XmlAdapter.cs
+++ spaceshipFactory/FileSupport/XmlAdapter.cs
@@ -1,6 +1,20 @@
+using System.Xml.Serialization;
+
 namespace spaceshipFactory.FileSupport;
 
-public class XmlAdapter
+public class XmlFileAdapter : IFileAdapter
 {
-    
+    public List<string>? LoadCommands(string filePath)
+    {
+        var serializer = new XmlSerializer(typeof(List<string>));
+        using var reader = new StreamReader(filePath);
+        return (List<string>)serializer.Deserialize(reader)!;
+    }
+
+    public void SaveOutput(string filePath, List<string> output)
+    {
+        var serializer = new XmlSerializer(typeof(List<string>));
+        using var writer = new StreamWriter(filePath);
+        serializer.Serialize(writer, output);
+    }
 }
